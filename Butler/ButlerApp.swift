bdb6f4c9a297039a7035a9d512e92e18194c233d
giordano scalzo <giordano.scalzo@gmail.com>
2025-03-04 13:51:19 +0000

    Add menu bar functionality and update project settings for UI element

--- Butler/ButlerApp.swift
+++ Butler/ButlerApp.swift
@@ -9,9 +9,28 @@ import SwiftUI
 
 @main
 struct ButlerApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+    @State var currentNumber: String = "1"
+       
+       var body: some Scene {
+           MenuBarExtra(currentNumber, systemImage: "\(currentNumber).circle") {
+               Button("One") {
+                   currentNumber = "1"
+               }
+               .keyboardShortcut("1")
+
+               Button("Two") {
+                   currentNumber = "2"
+               }
+               .keyboardShortcut("2")
+
+               Button("Three") {
+                   currentNumber = "3"
+               }
+               .keyboardShortcut("3")
+               Divider()
+               Button("Quit") {
+                   NSApplication.shared.terminate(nil)
+               }.keyboardShortcut("q")
+           }
+       }
 }
