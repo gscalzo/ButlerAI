9e19d4575f740ed976f9c1e38f5ef4be5aa74f94
giordano scalzo <giordano.scalzo@gmail.com>
2025-03-04 13:39:53 +0000

    Initial Commit

---
+++ Butler/ButlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButlerApp.swift
+//  Butler
+//
+//  Created by Scalzo, Giordano on 04/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct ButlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ButlerTests/ButlerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ButlerTests.swift
+//  ButlerTests
+//
+//  Created by Scalzo, Giordano on 04/03/2025.
+//
+
+import Testing
+@testable import Butler
+
+struct ButlerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
